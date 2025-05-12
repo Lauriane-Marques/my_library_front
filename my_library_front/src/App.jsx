0933f0bacb696c5cbe83869cb82c22db74736443
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-05-12 12:29:58 +0200

    [UPDATE] void import

--- my_library_front/src/App.jsx
+++ my_library_front/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
 

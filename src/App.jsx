4b49fbe83d12f0415ef4c33ca440fb40513a8280
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-05-13 09:37:10 +0200

    [UPDATE] file configuration

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home'
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <LogIn/>
+  },
+  {
+    path: '/signup',
+    element: <SignUp/>
+  }
+])
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App

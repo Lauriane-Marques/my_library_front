c5784e9f8a6dc19d7c22a947f8b4d9d5112efbcb
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-05-14 11:14:32 +0200

    [UPDATE] vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react(), tailwindcss()],
   base: '/',
   server: {
